705d355efd0e41df9c8143288dc8f2dae10d018e
Antonia Baumann <antonia.baumann@microsoft.com>
2020-07-30 20:31:01 +0100

    Move to Handelbars (#1)
    * basic hbs
    
    * move graphs to hbs
    
    * add basic carrussell
    
    * minor fixes
    
    * clean up

---
+++ assets/js/handelbars.js
@@ -0,0 +1,60 @@
+let DATA = undefined;
+
+Handlebars.getTemplate = function (name) {
+  if (
+    Handlebars.templates === undefined ||
+    Handlebars.templates[name] === undefined
+  ) {
+    $.ajax({
+      url: "assets/templates/" + name + ".hbs",
+      success: function (data) {
+        if (Handlebars.templates === undefined) {
+          Handlebars.templates = {};
+        }
+        Handlebars.templates[name] = Handlebars.compile(data);
+      },
+      async: false,
+    });
+  }
+  return Handlebars.templates[name];
+};
+
+Handlebars.showPage = function (page, context=undefined) {
+  if(page === PAGE_HOME && !context){
+    context = homeContext(DATA.data_info.default_year)
+  }
+  const compliedPage = Handlebars.getTemplate(page);
+  var html = compliedPage(context);
+  $(PAGE_ID).html(html);
+}
+
+window.onload = function () {
+  Handlebars.registerHelper("money", (value) => {
+    return asMoney(value);
+  });
+
+  $(MENU_ID).html(Handlebars.getTemplate(MENU_TEMPLATE));
+
+  fetch(LOCAL_DATA_FILE)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      DATA = data;
+      Handlebars.showPage(PAGE_HOME)
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// helper function to format number as money
+function asMoney(value) {
+  var num = Number(value);
+  if (num >= 1e6) {
+    const unit = num.toString().split('.')[0].length;
+    const unitName = unit < 10 ? "million" : "billion";
+    num = (num / ("1e" + (unit-3))).toFixed(3) + " " + unitName;
+  }
+  return "€ " + num;
+}
