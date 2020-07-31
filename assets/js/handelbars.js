be5d8c8af0a08d8262447bcf5e183c12bafbc308
Niall R Murphy <niallm@gmail.com>
2020-07-31 01:51:44 +0100

    Update handelbars.js
    Add asHouses function.

--- assets/js/handelbars.js
+++ assets/js/handelbars.js
@@ -32,6 +32,10 @@ window.onload = function () {
   Handlebars.registerHelper("money", (value) => {
     return asMoney(value);
   });
+  
+  Handlebars.registerHelper("houses", (value) => {
+    return asHouses(value);
+  });
 
   $(MENU_ID).html(Handlebars.getTemplate(MENU_TEMPLATE));
 
@@ -57,4 +61,10 @@ function asMoney(value) {
     num = (num / ( unit < 10 ? 1e6 : 1e9)).toFixed(3) + " " + unitName;
   }
   return "€ " + num;
+}
+
+// Simple division, using quoted 250k figure as of mid-2020
+function asHouses(value) {
+  var num = Number(value);
+  return Math.round(num / 250000);
 }
